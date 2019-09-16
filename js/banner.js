77c45bc225aa0a8c1b7f63995e4d8e16f2a7675e
JulianLz96 <jj_lz96@hotmail.com>
2019-09-15 19:25:42 -0500

    Intent off another section

--- js/banner.js
+++ js/banner.js
@@ -1,10 +1,5 @@
 let video = document.getElementById('video_banner');
 
-var size = {
-    width: window.innerWidth || document.body.clientWidth,
-    height: window.innerHeight || document.body.clientHeight
-}
-
 if (screen.width > 600) {
     video.play();
 }
